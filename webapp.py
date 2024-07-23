28947cfb5bd172cea1aa057a97f68dfb59780a4f
PiranavanR <piranavan02072004@gmail.com>
2024-07-23 22:06:42 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,6 +12,5 @@ def voice_command():
     command = recognize()
     return jsonify({'command': command})
 
-if __name__ == '__main__':
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(debug=False)
+    
+  
